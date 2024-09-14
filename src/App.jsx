9653e82fa76ee8e86ff81c65c5e26bfe305a3629
Gordon06060 <gordonz0913@gmail.com>
2024-09-14 04:45:03 -0400

    Added Logo and Register components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
 import "./App.css";
+import Register from "./components /Register";
+import Logo from "./components /Logo";
 
 function App() {
   return (
     <>
-      <div>Hello World</div>
+      <div className="homepage-container">
+        <div className="logo-app">
+          <Logo />
+        </div>
+        <div className="register-app">
+          <Register />
+        </div>
+      </div>
 
-      <iframe 
-            href="https://www.mappedin.com/" 
-            title="Mappedin Map" 
-            name="Mappedin Map" 
-            allow="clipboard-write; web-share" 
-            width="100%" 
-            height="650" 
-            frameBorder="0" 
-            // style="border:0"
-            src="https://app.mappedin.com/map/66ce20fdf42a3e000b1b0545?embedded=true">
-      </iframe>
+      <section id="map">
+        <iframe
+          href="https://www.mappedin.com/"
+          title="Mappedin Map"
+          name="Mappedin Map"
+          allow="clipboard-write; web-share"
+          width="100%"
+          height="650"
+          frameBorder="0"
+          // style="border:0"
+          src="https://app.mappedin.com/map/66ce20fdf42a3e000b1b0545?embedded=true"
+        ></iframe>
+      </section>
     </>
   );
 }
