2c3766c6d0355ded2a1547341127284e5d51f7dc
Gordon06060 <gordonz0913@gmail.com>
2024-09-14 23:35:07 -0400

    Implemented Defang to deploy app.

--- back-end/server.js
+++ back-end/server.js
@@ -1,15 +1,44 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const twilio = require("twilio");
 
 const app = express();
 const port = 4000;
+
 // Middleware for parsing JSON bodies
 app.use(bodyParser.json());
 
 // Middleware for parsing URL-encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// CORS setup
+app.use(
+  cors({
+    origin: "https://www.loolooloo.co/send-sms",
+  })
+);
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+// Twilio setup
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const toNumber = process.env.TO_NUMBER;
+const fromNumber = process.env.FROM_NUMBER;
+const client = twilio(accountSid, authToken);
+const URL = "www.google.com";
+
+app.get("/send-sms", async (req, res) => {
+  const message = await client.messages.create({
+    body: URL,
+    from: fromNumber,
+    to: toNumber,
   });
+
+  console.log("SENT");
+
+  res.send("SEND");
+});
+
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server is running on port ${port}`);
+});
--- back-end/twilio.js
+++ back-end/twilio.js
@@ -1,34 +1,44 @@
-// Download the helper library from https://www.twilio.com/docs/node/install
-//import twilio from "twilio";
-const twilio = require("twilio");
-const express = require("express");
-const bodyParser = require("body-parser");
+// // Download the helper library from https://www.twilio.com/docs/node/install
+// //import twilio from "twilio";
+// const twilio = require("twilio");
+// const express = require("express");
+// const bodyParser = require("body-parser");
 
-const app = express();
-const port = 4000;
-// Middleware for parsing JSON bodies
-app.use(bodyParser.json());
+// const app = express();
+// const port = 4000;
+// const cors = require("cors");
 
-// Middleware for parsing URL-encoded bodies
-app.use(bodyParser.urlencoded({ extended: true }));
-// Find your Account SID and Auth Token at twilio.com/console
-// and set the environment variables. See http://twil.io/secure
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = twilio(accountSid, authToken);
-const URL = "example.com";
-app.get("/", async (req, res) => {
-  const message = await client.messages.create({
-    body: "Hi Nathan",
-    from: "+13305296053",
-    to: "+17785129210",
-  });
+// app.use(
+//   cors({
+//     origin: "https://www.loolooloo.co/send-sms",
+//   })
+// );
+// // Middleware for parsing JSON bodies
+// app.use(bodyParser.json());
 
-  console.log("SENT");
+// // Middleware for parsing URL-encoded bodies
+// app.use(bodyParser.urlencoded({ extended: true }));
+// // Find your Account SID and Auth Token at twilio.com/console
+// // and set the environment variables. See http://twil.io/secure
+// const accountSid = process.env.TWILIO_ACCOUNT_SID;
+// const authToken = process.env.TWILIO_AUTH_TOKEN;
+// const toNumber = process.env.TO_NUMBER;
+// const fromNumber = process.env.FROM_NUMBER;
+// const client = twilio(accountSid, authToken);
+// const URL = "www.google.com";
 
-  res.send("SEND");
-});
+// app.get("/send-sms", async (req, res) => {
+//   const message = await client.messages.create({
+//     body: URL,
+//     from: fromNumber,
+//     to: toNumber,
+//   });
 
-app.listen(port, () => {
-  console.log("STARTED");
-});
+//   console.log("SENT");
+
+//   res.send("SEND");
+// });
+
+// app.listen(port, () => {
+//   console.log("STARTED");
+// });
